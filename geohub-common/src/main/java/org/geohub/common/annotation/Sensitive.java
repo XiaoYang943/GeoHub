a35ca77bb2f8e6f6e2d6726237b7b6f5df24d1c1
XiaoYang943 <1352253543@qq.com>
2024-07-21 16:06:29 +0800

    init: first commit

---
+++ geohub-common/src/main/java/org/geohub/common/annotation/Sensitive.java
@@ -0,0 +1,24 @@
+package org.geohub.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.geohub.common.config.serializer.SensitiveJsonSerializer;
+import org.geohub.common.enums.DesensitizedType;
+
+/**
+ * 数据脱敏注解
+ *
+ * @author ruoyi
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@JacksonAnnotationsInside
+@JsonSerialize(using = SensitiveJsonSerializer.class)
+public @interface Sensitive
+{
+    DesensitizedType desensitizedType();
+}
---
+++ geohub-common/src/main/java/org/geohub/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package org.geohub.common.exception.user;
+
+import org.geohub.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
